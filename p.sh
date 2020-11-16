792d0bc9688ee4ed5a4dfe63e5bc860ed7b8751b
Yury Sukhoverkhov <y.sukhoverkhov@newmotion.com>
2020-11-16 08:41:50 +0100

    qwe

--- p.sh
+++ p.sh
@@ -1,3 +1,3 @@
 pelican content -o output -s pelicanconf.py
-ghp-import output -b gh-pages
+ghp-import output -p -b gh-pages
 git push https://github.com/ysukhoverkhov/ysukhoverkhov.github.io.git gh-pages:master
